1ec35e733810cb5838c0470e2f5c9877076721b6
jose <knich0564@gmail.com>
2024-04-09 19:41:55 -0300

    Initial commit

---
+++ src/practico4/lab1c2/Practico4Lab1c2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practico4.lab1c2;
+
+import Formulario.Principal;
+
+/**
+ *
+ * @author jez_3
+ */
+public class Practico4Lab1c2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal p=new Principal();
+        p.setVisible(true);        
+    }
+    
+}
